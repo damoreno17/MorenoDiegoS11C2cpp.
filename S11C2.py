d4fbfb40b7a21cc6420ce3c7faf15ad1b16ce907
Diego <da.moreno17@uniandes.edu.co>
2019-04-04 11:11:21 -0700

    archivo en blanco

---
+++ S11C2.py
@@ -0,0 +1,17 @@
+## Semana 11 Clase 2
+#Ejercicio: Cree un repositorio llamado ApellidoNombreS11C2cpp. En este repositorio deben estar todos los codigos que debe entregar al final de la clase. la entrega debe ser un directorio comprimido ApellidoNombreS11C2cpp.zip que usted debe subir a sicua y el enlace de GitHub con el repositorio correspondiente.
+
+# programa 1: Haga un programa que reciba interactivamente dos valores enteros y retorne los numero primos contenidos en el intervalo delimitado por estos dos valores. llamelo primos.cpp
+
+# programa 2: Tome su programa que calcula el factorial de un numero n (y que recibe n interactivamente). Reescribalo usando funciones. La seccion de codigo que calcula el factorial debe ser una funcion independiente que es llamada desde la funcion main. llamelo factorial_interFUN.cpp
+
+# programa 3: Haga un programa que tenga dos funciones. Cada una debe recibir dos arreglos unidimensionales de 5 elementos cada uno. La primera funcion debe retornar el producto de los dos arreglos elemento a elemento. La segunda debe retornar el producto punto de los dos arreglos. En la funcion main se debe imprimir lo que retornen las dos funciones para [1,2,3,4,5] y [10,20,30,40,50]. llamelo producto_array_FUN.cpp
+
+# programa 4: repita su codigo del fatorial usando una funcion y recursividad.
+
+#por si acaban antes, para que o se aburran....
+# Programa n>5: Traduzca algunos de sus codigos de python a cpp: derivadas, integrales y MCMC. Para esto debe mirar antes los tutoriales cuyos enlaces estan en Sicua. 
+
+
+
+
