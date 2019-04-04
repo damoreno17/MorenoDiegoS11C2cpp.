d282dfb6a5cb3aca290443f74f88a48d7dc9d74e
Diego <da.moreno17@uniandes.edu.co>
2019-04-04 11:52:16 -0700

    primos que no sirve

---
+++ primos.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int primo (int u , int v);
+
+int main()
+{
+	int u;
+	int v;
+	cout << "numero minimo =";
+	 cin >> u ;
+	cout << "numero maximo ="; 
+	cin >> v ;
+
+	primo(u,v);
+
+return 0;
+}
+
+int primo(int u, int v)
+{
+	int i = 0;
+	int primo;
+	while (u<V)
+	{
+			
+
+	}
+}
